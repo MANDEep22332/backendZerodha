2c40874a5482dd548536ee4129583578df4873e7
Mandeep2333 <missiongamer15@gmail.com>
2025-12-04 10:04:31 +0530

    users

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ credentials: true// Allows cookies
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoute = require("./Routes/AuthRoute");
-const { HoldingModel } = require("./models/HoldingModel");
+const { HoldingModel } = require("./Models/HoldingModel");
 
-const { PositionsModel } = require("./models/PositionsModel").default;
-const { OrdersModel } = require("./models/OrdersModel");
+const { PositionsModel } = require("./Models/PositionsModel").default;
+const { OrdersModel } = require("./Models/OrdersModel");
 
 
 const PORT =  3002;
